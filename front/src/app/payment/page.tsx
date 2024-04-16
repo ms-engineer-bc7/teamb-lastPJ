823e99077ad7d17d533e4238db6e9a3ffeea08a7
tomoka <buru8buru8@gmail.com>
2024-04-16 22:34:42 +0900

    stripeフロントページ作成

---
+++ front/src/app/payment/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+import StripeBuyButton from "./stripebuybutton";
+
+import Head from 'next/head';
+
+export default function PaymentPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Payment</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-2xl font-bold">
+          Bu.Ra.Ri -さんぽっと- 
+        </h1>
+        <p className="mt-3 text-base">
+          Bu.Ra.Ri-さんぽっと　お支払いページ
+        </p>
+
+        <div className="mt-6">
+          <StripeBuyButton />
+        </div>
+      </main>
+    </div>
+  );
+}
--- front/src/app/payment/stripebuybutton.tsx
+++ front/src/app/payment/stripebuybutton.tsx
@@ -1,41 +1,77 @@
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image'; // Import the Image component
 
 const StripeBuyButton: React.FC = () => {
-  const [scriptLoaded, setScriptLoaded] = useState(false); //4/10追加
-  useEffect(() => {
-    // クライアントサイドでのみスクリプトを追加
+ const [scriptLoaded, setScriptLoaded] = useState<boolean>(false);
+
+ useEffect(() => {
     if (typeof window !== 'undefined') {
       const script = document.createElement('script');
       script.src = 'https://js.stripe.com/v3/buy-button.js';
       script.async = true;
-      script.onload = () => setScriptLoaded(true);//4/10追加
+      script.onload = () => setScriptLoaded(true);
       document.body.appendChild(script);
 
       return () => {
         document.body.removeChild(script);
       };
     }
-  }, []);
-  if (!scriptLoaded) {
-    return <div>Loading...</div>; // Or any loading indicator
+ }, []);
+
+ if (!scriptLoaded) {
+    return <div>Loading...</div>;
  }
-  return (
-    <stripe-buy-button
-      buy-button-id="buy_btn_1P3oEUCpbVKijddQ8Uu7qbPw"
-      publishable-key="pk_test_51P2Y0QCpbVKijddQnD0OHvWudxFghlcMlL0FMiDitKJMA0mRgyew2kzM9Bb6iRnjrYw52BJcdgxCANBrWUwGOKPs004wtvPlM6"
-    ></stripe-buy-button>
-  );
+
+ return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <stripe-buy-button
+        buy-button-id="buy_btn_1P3ZW1CpbVKijddQrZc406o7"
+        publishable-key="pk_test_51P2Y0QCpbVKijddQnD0OHvWudxFghlcMlL0FMiDitKJMA0mRgyew2kzM9Bb6iRnjrYw52BJcdgxCANBrWUwGOKPs004wtvPlM6"
+      >
+      </stripe-buy-button>
+      <Image
+        src="/qr_test_28o7su5tY9Cy1he3cd copy.png"
+        alt="QR Code"
+        width={200} // Adjust width as needed
+        height={200} // Adjust height as needed
+        style={{ marginLeft: '20px' }}
+      />
+    </div>
+ );
 };
 
 export default StripeBuyButton;
 
-// Stripeの公式のコード
-{/* <script async
-  src="https://js.stripe.com/v3/buy-button.js">
-</script>
 
-<stripe-buy-button
-  buy-button-id="buy_btn_1P3oEUCpbVKijddQ8Uu7qbPw"
-  publishable-key="pk_test_51P2Y0QCpbVKijddQnD0OHvWudxFghlcMlL0FMiDitKJMA0mRgyew2kzM9Bb6iRnjrYw52BJcdgxCANBrWUwGOKPs004wtvPlM6"
->
-</stripe-buy-button> */}
+// import React, { useEffect, useState } from 'react';
+
+// const StripeBuyButton: React.FC = () => {
+//   const [scriptLoaded, setScriptLoaded] = useState(false); //4/10追加
+//   useEffect(() => {
+//     // クライアントサイドでのみスクリプトを追加
+//     if (typeof window !== 'undefined') {
+//       const script = document.createElement('script');
+//       script.src = 'https://js.stripe.com/v3/buy-button.js';
+//       script.async = true;
+//       script.onload = () => setScriptLoaded(true);//4/10追加
+//       document.body.appendChild(script);
+
+//       return () => {
+//         document.body.removeChild(script);
+//       };
+//     }
+//   }, []);
+//   if (!scriptLoaded) {
+//     return <div>Loading...</div>; // Or any loading indicator
+//  }
+//   return (
+//     <stripe-buy-button
+//       buy-button-id="buy_btn_1P3ZW1CpbVKijddQrZc406o7"
+//       publishable-key="pk_test_51P2Y0QCpbVKijddQnD0OHvWudxFghlcMlL0FMiDitKJMA0mRgyew2kzM9Bb6iRnjrYw52BJcdgxCANBrWUwGOKPs004wtvPlM6"
+//       >
+//     </stripe-buy-button>
+//   );
+// };
+
+// export default StripeBuyButton;
+
